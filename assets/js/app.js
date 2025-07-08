a9ad44b9a884433d268abcaeb66266ec0872fdf4
jadzia-0 <jadzia.github@gmail.com>
2025-07-08 11:28:33 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,6 @@
-
+//Loading header.html and footer.html in JS DOM
+document.addEventListener("DOMContentLoaded", async (event) => {
+    loadComponent("header", "./components/header.html");
+    loadComponent("footer", "./components/footer.html");
+    console.log("DOM fully loaded and components loaded.");
+  });
