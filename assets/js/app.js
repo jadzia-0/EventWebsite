1155dc6cf9f47c6e6f13f3d2f0712f24f4e9d821
jadzia-0 <jadzia.github@gmail.com>
2025-07-08 12:13:53 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 //Loading header.html and footer.html in JS DOM
 document.addEventListener("DOMContentLoaded", async (event) => {
-    loadComponent("header", "../components/header.html");
+    loadComponent("header", "components/header.html"
+"./components/header.html"
+"../components/header.html");  // if HTML is nested deeper
+    
     loadComponent("footer", "../components/footer.html");
     console.log("DOM fully loaded and components loaded.");
   });
